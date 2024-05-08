8b4ec42227af9c87cf18b62b98e459d4762471d1
Aleksandar Pavlovic <aleksandar.pavlovic@vega.local>
2024-05-08 14:25:43 +0200

    feat:scraping constructors and races

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -4,24 +4,21 @@ import psycopg2 # type: ignore
 import requests
 
 # Initialize Kafka consumer
-consumer_circuit = KafkaConsumer('data_topic', bootstrap_servers='localhost:9092',
-                         value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+consumer = KafkaConsumer('data_topic', bootstrap_servers='localhost:9092',
+                         value_deserializer=lambda x: json.loads(x.decode('utf-8')),auto_offset_reset="latest")
 
 
-print("Consumer circuit is",consumer_circuit)
-
-# Subscribe to the topic
-
-
-
-
+print("Consumer circuit is",consumer)
 
 # Continuously consume messages from the topic
-def insert_circuit_data(consumer_circuit):
-    # consumer_circuit.subscribe(['data_topic'])
+def insert_data(consumer):
+    consumer.subscribe(['data_topic'])
     
     conn=None
+    driver_id = 1000
+    constructor_Id = 300
     race_number = 1
+    race_Id = 1500
     
     try:
         conn = psycopg2.connect(
@@ -33,8 +30,8 @@ def insert_circuit_data(consumer_circuit):
                 )
         cursor = conn.cursor()
         
-        for message in consumer_circuit:
-            print("Poruka je",message.value)
+        for message in consumer:
+            print("Poruka je")
             #If type of message is circuit
              
             if message.value['type']=="circuit":
@@ -77,7 +74,7 @@ def insert_circuit_data(consumer_circuit):
             #If type of message is driver    
             if message.value['type']=="driver":  
                 print("USLO U DRIVER")
-                driver_id = 1000
+                
                 
 
                 message_data = message.value['data']
@@ -110,10 +107,107 @@ def insert_circuit_data(consumer_circuit):
                     # Print additional details
                     print("Inserted new driver:", driver_ref)
 
-                # Continue processing next race data
+            # If the type of kafka message is constructor
+
+            if message.value['type']=="constructor":  
+                print("USLO U CONSTRUCTOR")
+
+                
                 
 
-            
+                message_data = message.value['data']
+                # print("Podaci su",message_data)
+                # Access fields within the message data
+                constructor_ref = message_data['constructor_ref']
+                name = message_data['name']
+                nationality = message_data['nationality']
+                
+
+                # Check if circuit exists in the database
+                cursor.execute('SELECT * FROM constructor WHERE "constructorRef" = %s', (constructor_ref,))
+                result = cursor.fetchone()
+                if not result:
+                    print("Uslo u no result",name)
+                    # Insert new constructor into the database
+                    cursor.execute("""
+                        INSERT INTO constructor ("constructorId","constructorRef", "name", "nationality")
+                        VALUES (%s, %s, %s,%s)
+                    """, (constructor_Id,constructor_ref, name, nationality))
+
+                    # Commit changes to the database
+                    conn.commit()
+                    constructor_Id=constructor_Id+1
+                    
+
+                    # Print additional details
+                    print("Inserted new constructor:", name)
+            # If the type of message is race
+            if message.value['type'] == "race":  
+                print("USLO U Race")
+
+                
+                
+
+                message_data = message.value['data']
+
+                print("Podaci su",message_data)
+                # Access fields within the message data
+                year = message_data['season']
+                round = message_data['round']
+                fp1Date = message_data.get('fp1Date') if message_data.get('fp1Date') != '' else None
+                fp1Time = message_data.get('fp1Time') if message_data.get('fp1Time') != '' else None
+                fp2Date = message_data.get('fp2Date') if message_data.get('fp2Date') != '' else None
+                fp2Time = message_data.get('fp2Time') if message_data.get('fp2Time') != '' else None
+                fp3Date = message_data.get('fp3Date') if message_data.get('fp3Date') != '' else None
+                fp3Time = message_data.get('fp3Time') if message_data.get('fp3Time') != '' else None
+                qualiDate = message_data.get('qualiDate') if message_data.get('qualiDate') != '' else None
+                qualiTime = message_data.get('qualiTime') if message_data.get('qualiTime') != '' else None
+                sprintDate = message_data.get('sprintDate') if message_data.get('sprintDate') != '' else None
+                sprintTime = message_data.get('sprintTime') if message_data.get('sprintTime') != '' else None
+                date = message_data['date']
+                time = message_data['time']
+                circuitName=message_data['circuitName']
+                
+                cursor.execute("SELECT * FROM race WHERE year = %s AND round = %s", (year,round,))
+                race_exists=cursor.fetchone()
+                print("Trka postoji",race_exists)
+                if not race_exists:
+                # Check if circuit exists in the database
+                    cursor.execute("SELECT * FROM circuit WHERE name_y = %s", (circuitName,))
+                    circuit_id = cursor.fetchone()
+                    print("Ime staze je",circuit_id)
+                    if circuit_id:
+                        circuit_id = circuit_id[0]
+
+                        cursor.execute("""
+                            INSERT INTO race ("raceId", "circuitId", "year", "round", "fp1Date", "fp1Time", "fp2Date", "fp2Time", "fp3Date", "fp3Time", "qualiDate", "qualiTime", "sprintDate", "sprintTime", "date", "time")
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                            """,(
+                                    race_Id,
+                                    circuit_id,
+                                    year,
+                                    round,
+                                    fp1Date,
+                                    fp1Time,
+                                    fp2Date,
+                                    fp2Time,
+                                    fp3Date,
+                                    fp3Time,
+                                    qualiDate,
+                                    qualiTime,
+                                    sprintDate,
+                                    sprintTime,
+                                    date,
+                                    time
+                                ))
+                        
+                        conn.commit()
+
+                        # Print additional details
+                        print("Inserted new race:", race_Id)
+                        race_Id=race_Id+1
+                
+
     except Exception as error:
             print("Error:", error)
     finally:
@@ -123,5 +217,6 @@ def insert_circuit_data(consumer_circuit):
                 conn.close()
                 print("PostgreSQL connection is closed")
 
-insert_circuit_data(consumer_circuit)
+
+insert_data(consumer)
 
--- kafka_scrape.py
+++ kafka_scrape.py
@@ -1,3 +1,4 @@
+import time
 import requests
 from kafka import KafkaProducer
 import json
@@ -78,7 +79,7 @@ def scraping_drivers_and_publishing_to_kafka(producer):
                     surname = driver_data['familyName']
                     dob = driver_data['dateOfBirth']
                     nationality = driver_data['nationality']
-
+                    
                     # Create a dictionary representing the racer data
                     driver = {
                         "driverRef":driver_ref,
@@ -89,13 +90,9 @@ def scraping_drivers_and_publishing_to_kafka(producer):
                         "dob":dob,
                         "nationality":nationality
                     }
-
                     # Publish circuit data to Kafka
                     producer.send('data_topic', value={"type":"driver","data":driver})
                     print("Published driver data to Kafka:", driver)
-
-                    
-                   
 
                 # Continue processing next race data
                 print("Race number is", race_number)
@@ -106,10 +103,119 @@ def scraping_drivers_and_publishing_to_kafka(producer):
     except Exception as error:
         print("Error:", error)
 
+
+def scraping_constructors_and_publishing_to_kafka(producer):
+    
+    try:
+        race_number = 1
+        while True:
+            url = f"http://ergast.com/api/f1/2024/{race_number}/constructors.json"
+            response = requests.get(url)
+            data = response.json()
+
+            if 'MRData' in data and 'ConstructorTable' in data['MRData']:
+                constructors = data['MRData']['ConstructorTable']['Constructors']
+
+                if not constructors:  # Check if constructors data is empty
+                    print(f"No constructor data found for the given year and race number {race_number}. Exiting loop.")
+                    break
+
+                for constructor_data in constructors:
+                    
+                    constructor_ref = constructor_data['constructorId']
+                    name = constructor_data['name']
+                    nationality = constructor_data['nationality']
+
+                    # Check if constructor exists in the database
+                    constructor = {    
+                            "constructor_ref":constructor_ref,
+                            "name": name,
+                            "nationality":nationality,    
+                    }
+                    producer.send('data_topic', {"type":"constructor","data":constructor})
+                    print("Published constructor data to Kafka:", constructor)
+
+                # Continue processing next race data
+                print("Race number is", race_number)
+                race_number += 1
+            else:
+                print("Failed to fetch data from the API.")
+                break
+    except Exception as error:
+        print("Error:", error)
+
+def scraping_race_and_publishing_to_kafka(producer):
+    
+    try:
+        race_number = 1
+        url = f"http://ergast.com/api/f1/2024.json"
+        response = requests.get(url)
+        data = response.json()
+        
+        if 'MRData' in data and 'RaceTable' in data['MRData']:
+            race_table = data['MRData']['RaceTable']
+            race_data = race_table.get('Races', [])
+
+            if not race_data:  # Check if "Races" is empty
+                print(f"No race data found for the given year and race number {race_number}. Exiting loop.")
+                
+            
+            for race in race_data:
+                season = race['season']
+                round_number = race['round']
+                race_date = race['date']
+                race_time = race['time']
+                circuit_name = race['Circuit']['circuitName']
+                fp1_date = race['FirstPractice']['date']
+                fp1_time = race['FirstPractice']['time']
+                fp2_date = race.get('SecondPractice', {}).get('date', '')
+                fp2_time = race.get('SecondPractice', {}).get('time', '')
+                fp3_date = race.get('ThirdPractice', {}).get('date', '')
+                fp3_time = race.get('ThirdPractice', {}).get('time', '')
+                quali_date = race['Qualifying']['date']
+                quali_time = race['Qualifying']['time']
+                sprint_date = race.get('Sprint', {}).get('date', '')
+                sprint_time = race.get('Sprint', {}).get('time', '')
+
+                race_object = {
+                    "season": season,
+                    "round": round_number,
+                    "fp1Date": fp1_date,
+                    "fp1Time": fp1_time,
+                    "fp2Date": fp2_date,
+                    "fp2Time": fp2_time,
+                    "fp3Date": fp3_date,
+                    "fp3Time": fp3_time,
+                    "qualiDate": quali_date,
+                    "qualiTime": quali_time,
+                    "sprintDate": sprint_date,
+                    "sprintTime": sprint_time,
+                    "date": race_date,
+                    "time": race_time,
+                    "circuitName": circuit_name
+                }
+
+                producer.send('data_topic', {"type":"race","data":race_object},partition=0)
+                producer.flush()
+                # time.sleep(0.01)
+                print("Published constructor data to Kafka:", race_object)
+
+                # Now you can use the race_object as you wish, such as publishing it to Kafka
+                # For example:
+                # producer.publish_race(race_object)
+
+                # print("Race:", race_object)
+
+            race_number += 1
+                
+    except Exception as error:
+        print("Error:", error)
 # Initialize Kafka producer
 producer = KafkaProducer(bootstrap_servers='localhost:9092',
                          value_serializer=lambda x: json.dumps(x).encode('utf-8'))
 
 # Call the function to scrape data and publish to Kafka
-scraping_circuits_and_publishing_to_kafka(producer)
-scraping_drivers_and_publishing_to_kafka(producer)
+# scraping_circuits_and_publishing_to_kafka(producer)
+# scraping_drivers_and_publishing_to_kafka(producer)
+# scraping_constructors_and_publishing_to_kafka(producer)
+scraping_race_and_publishing_to_kafka(producer)
